30a7da0c4ae60ecd011ba7d3d40413db1b1112ad
teju10 <tejsdani.infograins@gmail.com>
2018-09-19 17:24:21 -0700

    'Push_Notify'

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,80 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+	import { Component } from '@angular/core';
+	import { NavController } from 'ionic-angular';
+	import { Push, PushObject, PushOptions } from '@ionic-native/push';
+    import { AlertController } from 'ionic-angular';
+	
+	@Component({
+	  selector: 'page-home',
+	  templateUrl: 'home.html'
+	})
+	export class HomePage {
 
-@Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
-})
-export class HomePage {
+	  constructor(public navCtrl: NavController,private push: Push,private alert:AlertController) {
 
-  constructor(public navCtrl: NavController) {
+	  this.push.hasPermission()
+	  .then((res: any) => {
 
-  }
+		if (res.isEnabled) {
+		  console.log('We have permission to send push notifications');
+		  this.initPush();
+		} else {
+		  console.log('We do not have permission to send push notifications');
+		}
 
-}
+	  });
+
+	  
+	  }
+	  
+	  initPush(){
+		  
+		const options: PushOptions = {
+		android: {},
+		ios: {
+		   alert: 'true',
+		   badge: true,
+		   sound: 'false'
+	   },
+		 windows: {},
+		 browser: {
+		   pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+		}
+	};
+	 
+	const pushObject: PushObject = this.push.init(options);
+	
+	pushObject.on('notification').subscribe((notification: any) => 
+	{
+
+     let alert = this.alert.create({
+     title: 'Confirm purchase',
+     message: notification.message,
+     buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'See',
+        handler: () => {
+         this.navCtrl.parent.select(1);
+          console.log('Buy clicked');
+        }
+      }
+     ]
+     });
+     alert.present();
+
+	}
+	);
+    pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
+    pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+		  
+	  }
+
+	 
+	  
+	} 
